8c4990dda625b6874054b4edf18cf3ede70c53ff
Nikola Milivojevic <0dziga0@gmail.com>
2014-12-29 19:42:48 +0100

    frontend start

---
+++ public/javascripts/app.js
@@ -0,0 +1,14 @@
+(function(){
+	var app = angular.module('ispuni-dan', [ ]);
+	var activity = [{name:'yeah'}, {name:'dah'}];
+	
+	app.controller('MainController', ['$http', function($http){
+		var main = this;
+		
+		main.activities = activity;
+		$http.get('/activity-types').success(function(data){
+			main.activities = data;
+		});
+	}]);
+	
+})();
