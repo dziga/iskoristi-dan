10a94ef66be67933b89e68dbf40e5eece75ed0e4
Nikola Milivojevic <0dziga0@gmail.com>
2015-01-09 14:25:48 +0100

    sql

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -4,43 +4,43 @@
 # --- !Ups
 
 create table activity (
-  id                        bigint auto_increment not null,
+  id                        bigint not null,
   name                      varchar(255),
-  description               longtext,
+  description               clob,
   type_id                   bigint,
-  active                    tinyint(1) default 0,
+  active                    boolean,
   constraint uq_activity_name unique (name),
   constraint pk_activity primary key (id))
 ;
 
 create table activity_type (
-  id                        bigint auto_increment not null,
+  id                        bigint not null,
   name                      varchar(255),
   constraint uq_activity_type_name unique (name),
   constraint pk_activity_type primary key (id))
 ;
 
 create table image (
-  id                        bigint auto_increment not null,
+  id                        bigint not null,
   url                       varchar(255),
   constraint uq_image_url unique (url),
   constraint pk_image primary key (id))
 ;
 
 create table recommendation (
-  id                        bigint auto_increment not null,
+  id                        bigint not null,
   name                      varchar(255),
-  description               longtext,
-  start_time                datetime,
-  end_time                  datetime,
+  description               clob,
+  start_time                timestamp,
+  end_time                  timestamp,
   location                  varchar(255),
-  active                    tinyint(1) default 0,
+  active                    boolean,
   constraint uq_recommendation_name unique (name),
   constraint pk_recommendation primary key (id))
 ;
 
 create table tag (
-  id                        bigint auto_increment not null,
+  id                        bigint not null,
   name                      varchar(255),
   constraint uq_tag_name unique (name),
   constraint pk_tag primary key (id))
@@ -70,14 +70,24 @@ create table recommendation_image (
   image_id                       bigint not null,
   constraint pk_recommendation_image primary key (recommendation_id, image_id))
 ;
+create sequence activity_seq;
+
+create sequence activity_type_seq;
+
+create sequence image_seq;
+
+create sequence recommendation_seq;
+
+create sequence tag_seq;
+
 alter table activity add constraint fk_activity_type_1 foreign key (type_id) references activity_type (id) on delete restrict on update restrict;
 create index ix_activity_type_1 on activity (type_id);
 
 
 
-alter table activity_recommendation add constraint fk_activity_recommendation_activity_01 foreign key (activity_id) references activity (id) on delete restrict on update restrict;
+alter table activity_recommendation add constraint fk_activity_recommendation_ac_01 foreign key (activity_id) references activity (id) on delete restrict on update restrict;
 
-alter table activity_recommendation add constraint fk_activity_recommendation_recommendation_02 foreign key (recommendation_id) references recommendation (id) on delete restrict on update restrict;
+alter table activity_recommendation add constraint fk_activity_recommendation_re_02 foreign key (recommendation_id) references recommendation (id) on delete restrict on update restrict;
 
 alter table activity_tag add constraint fk_activity_tag_activity_01 foreign key (activity_id) references activity (id) on delete restrict on update restrict;
 
@@ -87,31 +97,41 @@ alter table activity_image add constraint fk_activity_image_activity_01 foreign 
 
 alter table activity_image add constraint fk_activity_image_image_02 foreign key (image_id) references image (id) on delete restrict on update restrict;
 
-alter table recommendation_image add constraint fk_recommendation_image_recommendation_01 foreign key (recommendation_id) references recommendation (id) on delete restrict on update restrict;
+alter table recommendation_image add constraint fk_recommendation_image_recom_01 foreign key (recommendation_id) references recommendation (id) on delete restrict on update restrict;
 
 alter table recommendation_image add constraint fk_recommendation_image_image_02 foreign key (image_id) references image (id) on delete restrict on update restrict;
 
 # --- !Downs
 
-SET FOREIGN_KEY_CHECKS=0;
+SET REFERENTIAL_INTEGRITY FALSE;
 
-drop table activity;
+drop table if exists activity;
 
-drop table activity_recommendation;
+drop table if exists activity_recommendation;
 
-drop table activity_tag;
+drop table if exists activity_tag;
 
-drop table activity_image;
+drop table if exists activity_image;
 
-drop table activity_type;
+drop table if exists activity_type;
 
-drop table image;
+drop table if exists image;
 
-drop table recommendation;
+drop table if exists recommendation;
 
-drop table recommendation_image;
+drop table if exists recommendation_image;
 
-drop table tag;
+drop table if exists tag;
 
-SET FOREIGN_KEY_CHECKS=1;
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists activity_seq;
+
+drop sequence if exists activity_type_seq;
+
+drop sequence if exists image_seq;
+
+drop sequence if exists recommendation_seq;
+
+drop sequence if exists tag_seq;
 
